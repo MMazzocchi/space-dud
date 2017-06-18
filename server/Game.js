7253b5f84ce8d10de70d5e048ad344d981ebf3c6
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2017-06-18 11:37:39 -0400

    Altered Game's serialization to include manager.

--- server/Game.js
+++ server/Game.js
@@ -18,11 +18,14 @@ Game.prototype.getPlayer = function(player_id) {
 };
 
 Game.prototype.toJSON = function() {
-  var info = {};
+  var info = {
+    'players': {},
+    'manager': this.manager
+  };
 
   for(var player_id in this.player_lookup) {
     var player = this.player_lookup[player_id];
-    info[player_id] = player;
+    info['players'][player_id] = player;
   }
 
   return info;
