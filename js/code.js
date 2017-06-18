0262d36f75cfb13398d07e214d450f09bcb1e193
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2017-06-18 11:57:46 -0400

    Dynamically made the roles table based on game data.

--- js/code.js
+++ js/code.js
@@ -1,8 +1,34 @@
 $(function() {
+  function load_roles_table(roles_data) {
+    var html = "";
+
+    if(!roles_data.manager) {
+      html += '<div><a class="btn btn-default role-btn">Manager</a></div>';
+    }
+
+    var players = roles_data.players;
+    for(var id in players) {
+      var player = players[id];
+      if(!player.display) {
+        html += '<div><a class="btn btn-default role-btn">Player #'+id+
+                ' - Display</a></div>';
+      }
+
+      if(!player.controller) {
+        html += '<div><a class="btn btn-default role-btn">Player #'+id+
+                ' - Controller</a></div>';
+      }
+    }
+
+    html += '<div><a class="btn btn-default role-btn">Add Player</a></div>';
+
+    $('#content')[0].innerHTML = html;
+  }
+
   var socket = io();
 
   socket.on('game_data', function(data) {
-    console.log(data);
+    load_roles_table(data);
   });
 
   $('.role-btn').click(function(e) {
