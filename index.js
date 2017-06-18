3d8244e025b80c5618549d4289929e81c26a51ad
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2017-06-18 10:38:45 -0400

    Added js folder, and a Game object.

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); 
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); 
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); 
+
+app.use('/js', express.static(__dirname + '/js')); 
 app.use('/css', express.static(__dirname + '/css'));
 
 app.get('/', function(req, res){
---
+++ js/Game.js
@@ -0,0 +1,18 @@
+var Game = function() {
+  this.manager = undefined;
+  this.player_lookup = {};
+
+  this.next_player_id = 0;
+};
+
+Game.prototype.addPlayer = function(player) {
+  var player_id = this.next_player_id;
+  this.player_lookup[player_id] = player;
+
+  this.next_player_id += 1;
+  return player_id;
+};
+
+Game.prototype.getPlayer = function(player_id) {
+  return this.player_lookup[player_id];
+};
