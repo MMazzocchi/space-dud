94084068e32cbd59bfbda123e88fa2ca695ba793
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2017-06-20 17:09:16 -0400

    Displayed current players in display client menu.

--- display_client/js/display.js
+++ display_client/js/display.js
@@ -1,5 +1,18 @@
 $(function() {
+  function setupPlayerList(player_list) {
+    var html = "";
+    for(var i = 0; i < player_list.length; i++) {
+      var player_id = player_list[i];
+      html += '<div><a class="btn btn-default">'+player_id+'</a></div>';
+    }
+
+    $('#status')[0].innerHTML = "Select your player ID:";
+    $('#content')[0].innerHTML = html;
+  };
+
   var socket = io();
+
+  socket.on('player_list', setupPlayerList);
   
   socket.emit('set_role', 'display');
 });
