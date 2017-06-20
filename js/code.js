bda8ec354b86ed0b3b18fd47c6f35371e1005571
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2017-06-20 18:14:49 -0400

    Removed old code files.

--- js/code.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-  var socket = io();
-
-  socket.on('game_data', function(data) {
-    load_roles_table(data, socket);
-  });
-});
--- js/display_client.js
+++
@@ -1,7 +0,0 @@
-function setup_display_client(socket) {
-  console.log("Setting up display client.");
-
-  socket.on('controller_event', function(data) {
-    console.log(data);
-  });
-}
