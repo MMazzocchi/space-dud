e984de92bb9865b04066e5a317c53d36d78e64ae
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2017-06-20 17:49:06 -0400

    Added ability to set a callback for any event.

--- display_client/js/DisplayClient.js
+++ display_client/js/DisplayClient.js
@@ -1,11 +1,18 @@
 var DisplayClient = function(socket) {
   this.socket = socket;
+  this.anyChangeCallback = undefined;
   var client = this;
   socket.on('controller_event', function(data) {
     client.processEvent(data);
   });
 };
 
+DisplayClient.prototype.onAnyChange = function(callback) {
+  this.anyChangeCallback = callback;
+};
+
 DisplayClient.prototype.processEvent = function(data) {
-  console.log(data);
+  if(this.anyChangeCallback != undefined) {
+    this.anyChangeCallback(data);
+  }
 };
