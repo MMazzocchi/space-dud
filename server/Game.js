67faf4fb5ea232d544c2694d3cd120c76f3d63c9
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2017-06-19 21:59:05 -0400

    Gave players random-ish looking ids.

--- server/Game.js
+++ server/Game.js
@@ -1,12 +1,20 @@
 var Game = function() {
+  this.PLAYER_ID_INTERVAL = 100000+Math.floor(Math.random()*100000);
+
   this.manager = null;
   this.player_lookup = {};
 
-  this.next_player_id = 0;
+  this.next_player_id = Date.now() + this.PLAYER_ID_INTERVAL;
 };
 
+Game.prototype.getNewPlayerId = function() {
+  var id = this.next_player_id;
+  this.next_player_id += this.PLAYER_ID_INTERVAL;
+  return id;
+}
+
 Game.prototype.addPlayer = function(player) {
-  var player_id = this.next_player_id;
+  var player_id = this.getNewPlayerId();
   this.player_lookup[player_id] = player;
 
   this.next_player_id += 1;
