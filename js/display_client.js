3bf011e010c4ba9219171e7f646e68002d583b67
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2017-06-19 20:50:08 -0400

    Fixed display client event name.

--- js/display_client.js
+++ js/display_client.js
@@ -1,7 +1,7 @@
 function setup_display_client(socket) {
   console.log("Setting up display client.");
 
-  socket.on('game_event', function(data) {
+  socket.on('controller_event', function(data) {
     console.log(data);
   });
 }
