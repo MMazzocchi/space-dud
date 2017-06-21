e21456cbe3b2580bb69f71b35806a0b7b1524637
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2017-06-21 16:59:46 -0400

    Moved all socket code into DisplayClient. Moved all UI code into display.

--- display_client/js/DisplayClient.js
+++ display_client/js/DisplayClient.js
@@ -1,18 +1,38 @@
-var DisplayClient = function(socket) {
-  this.socket = socket;
-  this.anyChangeCallback = undefined;
+var DisplayClient = function() {
+  this.socket = io();
+
   var client = this;
-  socket.on('controller_event', function(data) {
+  this.socket.on('player_list', function(player_list) {
+    client.processPlayerList(player_list);
+  });  
+  this.socket.on('controller_event', function(data) {
     client.processEvent(data);
   });
+
+  this.socket.emit('set_role', 'display');
 };
 
 DisplayClient.prototype.onAnyChange = function(callback) {
   this.anyChangeCallback = callback;
 };
 
+DisplayClient.prototype.onPlayerList = function(callback) {
+  this.playerListCallback = callback;
+};
+
 DisplayClient.prototype.processEvent = function(data) {
   if(this.anyChangeCallback != undefined) {
     this.anyChangeCallback(data);
   }
+};
+
+DisplayClient.prototype.processPlayerList = function(player_list) {
+  if(this.playerListCallback) {
+    this.playerListCallback(player_list);
+  }
+};
+
+DisplayClient.prototype.selectPlayer = function(player_id) {
+  this.player_id = player_id;
+  this.socket.emit('choose_player', player_id);
 };
--- display_client/js/display.js
+++ display_client/js/display.js
@@ -1,9 +1,11 @@
 $(function() {
-  function setupPlayerList(player_list, socket) {
+  function setupPlayerList(player_list, client) {
+
     var html = "";
     for(var i = 0; i < player_list.length; i++) {
-      var player_id = player_list[i];
-      html += '<div><a class="btn btn-default choose-player-btn">'+player_id+'</a></div>';
+      html += '<div><a class="btn btn-default choose-player-btn">'+
+              player_list[i]+
+              '</a></div>';
     }
 
     $('#status')[0].innerHTML = "Select your player ID:";
@@ -16,19 +18,16 @@ $(function() {
       $('#content')[0].innerHTML = "";
       $('#status')[0].innerHTML = "Waiting for events...";
 
-      var client = new DisplayClient(socket);
-      client.onAnyChange(function(data) {
-        $('#status')[0].innerHTML = "Received event: "+JSON.stringify(data);
-      });
-
-      socket.emit('choose_player', player_id);
+      client.selectPlayer(player_id);
     });
   };
 
-  var socket = io();
+  var client = new DisplayClient();
+  client.onAnyChange(function(data) {
+    $('#status')[0].innerHTML = "Received event: "+JSON.stringify(data);
+  });
 
-  socket.on('player_list', function(player_list) {
-    setupPlayerList(player_list, socket);
-  });  
-  socket.emit('set_role', 'display');
+  client.onPlayerList(function(player_list) {
+    setupPlayerList(player_list, client);
+  });
 });
