60b142ab6b803d4fcf3a5b6e1ae62163ded631c5
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2017-07-05 19:27:40 -0400

    Wrapped DisplayClient in a function.

--- js/DisplayClient.js
+++ js/DisplayClient.js
@@ -1,83 +1,89 @@
-var DisplayClient = function() {
-  this.state = {
-    button: {},
-    axis: {}
-  };
+var DisplayClient = (function() {
 
-  this.callbacks = {
-    button: {},
-    axis: {}
-  };
-
-  this.socket = io();
-
-  var client = this;
-  this.socket.on('controller_event', function(data) {
-    client.processEvent(data);
-  });
-
-  this.socket.emit('set_role', 'display');
-};
-
-DisplayClient.prototype.onControllerEvent = function(type, id, callback) {
-  this.callbacks[type][id] = async function(data) {
-    callback(data);
-  };
-};
-
-DisplayClient.prototype.onAnyChange = function(callback) {
-  this.anyChangeCallback = async function(data) {
-    callback(data);
-  };
-};
-
-DisplayClient.prototype.processEvent = function(data) {
-  this.state[data.type][data.id] = data.value;
-
-  if(this.callbacks[data.type][data.id]) {
-    this.callbacks[data.type][data.id](data);
-  }
-
-  if(this.anyChangeCallback != undefined) {
-    this.anyChangeCallback(data);
-  }
-};
-
-DisplayClient.prototype.selectPlayer = function(player_id, callback) {
-  if(this.choosePlayerCallback == undefined) {
+  function DisplayClient() {
+    this.state = {
+      button: {},
+      axis: {}
+    };
+  
+    this.callbacks = {
+      button: {},
+      axis: {}
+    };
+  
+    this.socket = io();
+  
     var client = this;
-    this.socket.on('valid_player_choice', function(valid) {
-      if(valid) {
-        client.player_id = player_id;
-      }
-
-      client.choosePlayerCallback(valid);
+    this.socket.on('controller_event', function(data) {
+      client.processEvent(data);
     });
-  }
-  this.choosePlayerCallback = callback;
+  
+    this.socket.emit('set_role', 'display');
+  };
+  
+  DisplayClient.prototype.onControllerEvent = function(type, id, callback) {
+    this.callbacks[type][id] = async function(data) {
+      callback(data);
+    };
+  };
+  
+  DisplayClient.prototype.onAnyChange = function(callback) {
+    this.anyChangeCallback = async function(data) {
+      callback(data);
+    };
+  };
+  
+  DisplayClient.prototype.processEvent = function(data) {
+    this.state[data.type][data.id] = data.value;
+  
+    if(this.callbacks[data.type][data.id]) {
+      this.callbacks[data.type][data.id](data);
+    }
+  
+    if(this.anyChangeCallback != undefined) {
+      this.anyChangeCallback(data);
+    }
+  };
+  
+  DisplayClient.prototype.selectPlayer = function(player_id, callback) {
+    if(this.choosePlayerCallback == undefined) {
+      var client = this;
+      this.socket.on('valid_player_choice', function(valid) {
+        if(valid) {
+          client.player_id = player_id;
+        }
+  
+        client.choosePlayerCallback(valid);
+      });
+    }
+    this.choosePlayerCallback = callback;
+  
+    this.socket.emit('choose_player', player_id);
+  };
+  
+  DisplayClient.prototype.getEventTypes = function() {
+    var types = [];
+    for(var id in this.state.button) {
+      types.push({
+        type: 'button',
+        id: id
+      });
+    }
+  
+    for(var id in this.state.axis) {
+      types.push({
+        type: 'axis',
+        id: id
+      });
+    }
+  
+    return types;
+  };
+  
+  DisplayClient.prototype.getState = function(type, id) {
+    return this.state[type][id];
+  };
 
-  this.socket.emit('choose_player', player_id);
-};
+  return DisplayClient;
+})();
 
-DisplayClient.prototype.getEventTypes = function() {
-  var types = [];
-  for(var id in this.state.button) {
-    types.push({
-      type: 'button',
-      id: id
-    });
-  }
-
-  for(var id in this.state.axis) {
-    types.push({
-      type: 'axis',
-      id: id
-    });
-  }
-
-  return types;
-};
-
-DisplayClient.prototype.getState = function(type, id) {
-  return this.state[type][id];
-};
