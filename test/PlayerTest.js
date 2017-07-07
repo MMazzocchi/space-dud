c7a79473b4501599ead13ad080df5f2fef562e5b
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2017-07-06 20:01:11 -0400

    Finished PlayerTest file.

--- test/PlayerTest.js
+++ test/PlayerTest.js
@@ -5,6 +5,14 @@ var DisplayClient = require('../server/DisplayClient.js');
 var ControllerClient = require('../server/ControllerClient.js');
 
 describe('Player', function() {
+  describe('#Player', function() {
+    it('should instantiate null for all clients.', function() {
+      var player = new Player();
+      assert(!player.hasDisplayClient());
+      assert(!player.hasControllerClient());
+    });
+  });
+
   describe('#hasDisplayClient', function() {
     it('should return false if no display client is set.', function() {
       var player = new Player();
@@ -34,6 +42,41 @@ describe('Player', function() {
 
       var display_client = new DisplayClient(test_socket);
       player.setDisplayClient(display_client);
+    });
+  });
+
+  describe('#hasControllerClient', function() {
+    it('should return false if no controller client is set.', function() {
+      var player = new Player();
+      assert(!player.hasControllerClient());
+    });
+
+    it('should return true if controller client is set.', function() {
+      var player = new Player();
+
+      var test_socket = new TestSocket();
+      var controller_client = new ControllerClient(test_socket);
+
+      player.setControllerClient(controller_client);
+      assert(player.hasControllerClient());
+    });
+  });
+
+  describe('#setControllerClient', function() {
+    it('should trigger a dump state if display client is set.',
+       function(done) {
+      var player = new Player();
+
+      var display_client = new DisplayClient(test_socket);
+      player.setDisplayClient(display_client);
+
+      var test_socket = new TestSocket();
+      test_socket.on('dump_state', function() {
+        done();
+      });
+
+      var controller_client = new ControllerClient(test_socket);
+      player.setControllerClient(controller_client);
     });
   });
 });
