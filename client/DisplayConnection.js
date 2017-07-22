3ab54ceb9928ea1f95397c5a174b4b3dfe500d97
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2017-07-21 20:27:05 -0400

    Made DisplayConnection a closure.

--- client/DisplayConnection.js
+++ client/DisplayConnection.js
@@ -1,80 +1,84 @@
-var DisplayConnection = (function() {
+var DisplayConnection = function() {
 
+  var that = {};
+
+  // Fields
+  var anyChangeCallback = undefined;
+  var choosePlayerCallback = undefined;
+  var player_id = undefined;
+
+  var state = {
+    button: {},
+    axis: {}
+  };
+  
+  var callbacks = {
+    button: {},
+    axis: {}
+  };
+  
+  var socket = io('/space-dud');
+  socket.on('controller_event', (data) => {
+    processEvent.call(that, data);
+  });
+  
+  // Private functions
   function processEvent(data) {
-    this.state[data.type][data.id] = data.value;
+    state[data.type][data.id] = data.value;
   
-    if(this.callbacks[data.type][data.id]) {
-      this.callbacks[data.type][data.id](data);
+    if(callbacks[data.type][data.id]) {
+      callbacks[data.type][data.id](data);
     }
   
-    if(this.anyChangeCallback !== undefined) {
-      this.anyChangeCallback(data);
+    if(anyChangeCallback !== undefined) {
+      anyChangeCallback(data);
     }
   };
-
-  function DisplayConnection() {
-    this.state = {
-      button: {},
-      axis: {}
-    };
-  
-    this.callbacks = {
-      button: {},
-      axis: {}
-    };
-  
-    this.socket = io('/space-dud');
-  
-    this.socket.on('controller_event', (data) => {
-      processEvent.call(this, data);
-    });
-  
-    this.socket.emit('set_role', 'display');
-  };
-  
-  DisplayConnection.prototype.onControllerEvent = function(type, id, callback) {
-    this.callbacks[type][id] = async function(data) {
+ 
+  // Public functions
+  that.onControllerEvent = function(type, id, callback) {
+    callbacks[type][id] = async function(data) {
       callback(data);
     };
 
-    return this;
+    return that;
   };
   
-  DisplayConnection.prototype.onAnyChange = function(callback) {
-    this.anyChangeCallback = async function(data) {
+  that.onAnyChange = function(callback) {
+    anyChangeCallback = async function(data) {
       callback(data);
     };
 
-    return this;
+    return that;
   };
    
-  DisplayConnection.prototype.selectPlayer = function(player_id, callback) {
-    if(this.choosePlayerCallback === undefined) {
-      this.socket.on('valid_player_choice', (valid) => {
+  that.selectPlayer = function(player_id, callback) {
+    if(choosePlayerCallback === undefined) {
+      socket.on('valid_player_choice', (valid) => {
         if(valid) {
-          this.player_id = player_id;
+          player_id = player_id;
         }
   
-        this.choosePlayerCallback(valid);
+        choosePlayerCallback(valid);
       });
     }
-    this.choosePlayerCallback = callback;
+    choosePlayerCallback = callback;
   
-    this.socket.emit('choose_player', player_id);
+    socket.emit('choose_player', player_id);
 
-    return this;
+    return that;
   };
   
-  DisplayConnection.prototype.getEventTypes = function() {
+  that.getEventTypes = function() {
     var types = [];
-    for(var id in this.state.button) {
+    for(var id in state.button) {
       types.push({
         type: 'button',
         id: id
       });
     }
   
-    for(var id in this.state.axis) {
+    for(var id in state.axis) {
       types.push({
         type: 'axis',
         id: id
@@ -84,10 +88,12 @@ var DisplayConnection = (function() {
     return types;
   };
   
-  DisplayConnection.prototype.getState = function(type, id) {
-    return this.state[type][id];
+  that.getState = function(type, id) {
+    return state[type][id];
   };
 
-  return DisplayConnection;
-})();
+  // After all instantiation, set the role to display
+  socket.emit('set_role', 'display');
 
+  return that;
+};
