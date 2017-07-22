eee8c7e5e4a4e1a3ba98c8eb5d6a2e4fdca0f701
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2017-07-21 20:58:47 -0400

    Wrapped Player in a closure.

--- server/Player.js
+++ server/Player.js
@@ -1,44 +1,49 @@
-var debug = require('debug')('space-dud:Player');
-
 var Player = function() {
+
+  var that = {};
+  var display_client = null;
+  var controller_client = null;
+
+  // Fields
+  var debug = require('debug')('space-dud:Player');
   debug('Created a new Player.');
 
-  this.display_client = null;
-  this.controller_client = null;
-}
-
-Player.prototype.setDisplayClient = function(client) {
-  debug('Connected a display client.');
-
-  this.display_client = client;
-
-  if(this.controller_client !== null) {
-    this.controller_client.setDisplayClient(client);
-    this.controller_client.dumpState();
+  // Public methods
+  that.setDisplayClient = function(client) {
+    debug('Connected a display client.');
+  
+    display_client = client;
+  
+    if(controller_client !== null) {
+      controller_client.setDisplayClient(client);
+      controller_client.dumpState();
+    }
+  
+    return that;
   }
-
-  return this;
-}
-
-Player.prototype.hasDisplayClient = function() {
-  return (this.display_client !== null);
-};
-
-Player.prototype.setControllerClient = function(client) {
-  debug('Connected a controller client.');
-
-  this.controller_client = client;
-
-  if(this.display_client !== null) {
-    this.controller_client.setDisplayClient(this.display_client);
-    this.controller_client.dumpState();
+  
+  that.hasDisplayClient = function() {
+    return (display_client !== null);
+  };
+  
+  that.setControllerClient = function(client) {
+    debug('Connected a controller client.');
+  
+    controller_client = client;
+  
+    if(display_client !== null) {
+      controller_client.setDisplayClient(display_client);
+      controller_client.dumpState();
+    }
+  
+    return this;
   }
+  
+  that.hasControllerClient = function() {
+    return (controller_client != null);
+  };
 
-  return this;
-}
-
-Player.prototype.hasControllerClient = function() {
-  return (this.controller_client != null);
+  return that;
 };
 
 module.exports = Player;
