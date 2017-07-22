557bffbf86ba1e8ae485bf7680ea6eb9a119a05e
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2017-07-21 20:55:56 -0400

    Wrapped GameServer in a closure and had it return something 'useful'.

--- server/GameServer.js
+++ server/GameServer.js
@@ -1,10 +1,17 @@
-var debug = require('debug')('space-dud:GameServer');
-var SocketServer = require('./SocketServer.js');
-var StaticServer = require('./StaticServer.js');
-
 var GameServer = function(http) {
-  this.static_server = new StaticServer(http);
-  this.socket_server = new SocketServer(http);
+
+  var that = {};
+
+  // Imports
+  var SocketServer = require('./SocketServer.js');
+  var StaticServer = require('./StaticServer.js');
+
+  // Fields
+  var debug = require('debug')('space-dud:GameServer');
+  var static_server = new StaticServer(http);
+  var socket_server = new SocketServer(http);
+
+  return that;
 };
 
 module.exports = GameServer; 
