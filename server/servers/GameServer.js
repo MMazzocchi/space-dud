2bb2a27e19612e069c34a744af4393c670a2cd16
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2017-08-06 19:57:54 -0400

    Moved game into GameServer.

--- server/servers/GameServer.js
+++ server/servers/GameServer.js
@@ -5,11 +5,14 @@ var GameServer = function(http) {
   // Imports
   var SocketServer = require('./SocketServer.js');
   var StaticServer = require('./StaticServer.js');
+  var Game = require('../Game.js');
 
   // Fields
   var debug = require('debug')('space-dud:GameServer');
+  var game = new Game();
+
   var static_server = new StaticServer(http);
-  var socket_server = new SocketServer(http);
+  var socket_server = new SocketServer(game, http);
 
   return that;
 };
