d467a612613f9ca3f2a1477a6309e8ffea4601b8
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2017-10-23 18:28:00 -0400

    Removed deprecated methods in Client.

--- server/clients/Client.js
+++ server/clients/Client.js
@@ -7,26 +7,6 @@ var Client = function(socket) {
     that.emit('disconnect', ...args);
   });
 
-  /**
-   * @deprecated
-   */
-  that.onDisconnect = function(...args) {
-    console.log('Client.onDisconnect is deprecated. Use Client.on('+
-                '"disconnect",...) instead. This method will be removed '+
-                'in release 2.6.0.');
-    that.on("disconnect", ...args);
-  };
-
-  /**
-   * @deprecated
-   */
-  that.triggerDisconnect = function(...args) {
-    console.log('Client.triggerDisconnect is deprecated. Use Client.emit('+
-                '"disconnect",...) instead. This method will be removed '+
-                'in release 2.6.0.');
-    that.emit("disconnect", ...args);
-  };
-
   return that;
 };
 
