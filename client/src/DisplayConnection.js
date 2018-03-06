8bce473b6b04918594c321415bd2939be03ed633
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2018-03-06 09:18:37 -0500

    Fixes #61 Removed deprecated DisplayConnection methods.

--- client/src/DisplayConnection.js
+++ client/src/DisplayConnection.js
@@ -32,42 +32,6 @@ var DisplayConnection = function() {
   };
  
   // Public functions
-
-  /**
-   * @deprecated
-   */
-  that.onEventType = function(event_type, callback) {
-    console.warn("DisplayConnection.onEventType is deprecated, and will be "+
-                 "removed in release 2.7.0. Use DisplayConnection.on(<event "+
-                 "type>, <callback>) instead.");
-    that.on(event_type, callback);
-    return that;
-  };
-
-  /**
-   * @deprecated
-   */
-  that.offEventType = function(event_type, callback) {
-    console.warn("DisplayConnection.offEventType is deprecated, and will be "+
-                 "removed in release 2.7.0. Use "+
-                 "DisplayConnection.removeListener(<event type>, <callback>) "+
-                 "instead.");
-    that.removeListener(event_type, callback);
-    return that;
-  };
-
-  /**
-   * @deprecated
-   */
-  that.clearEventType = function(event_type) {
-    console.warn("DisplayConnection.offEventType is deprecated, and will be "+
-                 "removed in release 2.7.0. Use "+
-                 "DisplayConnection.removeAllListeners(<event type>, "+
-                 "<callback>) instead.");
-    that.removeAllListeners(event_type);
-    return that;
-  };
-
   that.selectPlayer = function(selected_player_id) {
     player_id = selected_player_id; 
     socket.emit('choose_player', selected_player_id);
