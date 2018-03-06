82b5a86d42b9c670f0a10022bfdcf6cd1600ed08
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2018-03-06 11:22:26 -0500

    Created standalone file for space-dud client files.

---
+++ client/src/SpaceDudClient.js
@@ -0,0 +1,12 @@
+var SpaceDudClient = {
+  Connection:           require('./Connection.js'),
+
+  DisplayConnection:    require('./DisplayConnection.js'),
+
+  ControllerConnection: require('./ControllerConnection.js'),
+  GamepadConnection:    require('./GamepadConnection.js'),
+  KeyboardConnection:   require('./KeyboardConnection.js')
+};
+
+module.exports = SpaceDudClient;
+
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/bash
 
-src_files="./client/src/DisplayConnection.js \
-           ./client/src/ControllerConnection.js \
-           ./client/src/KeyboardConnection.js \
-           ./client/src/GamepadConnection.js"
+src_dir=./client/src/
+
+src_files="$src_dir/DisplayConnection.js \
+           $src_dir/ControllerConnection.js \
+           $src_dir/KeyboardConnection.js \
+           $src_dir/GamepadConnection.js"
+
 dist_dir=./client/dist
 
 if [[ ! -e $dist_dir ]]
 then
   mkdir $dist_dir
 fi
+
+rm -f $dist_dir/*.js $dist_dir/*.map
 
 for src_name in $src_files
 do
@@ -19,3 +24,9 @@ do
 
   browserify -s $class_name -e $src_name -o $dist_dir/$class_name.js
 done
+
+echo "Generating space-dud-client files..."
+
+browserify -s SpaceDudClient \
+           -e $src_dir/SpaceDudClient \
+           -o $dist_dir/space-dud-client.js
