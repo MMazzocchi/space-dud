b9de210a9667e581543210ec1cf3b5316e01be10
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2018-03-08 22:19:04 -0500

    Added test task to gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -2,4 +2,14 @@ var gulp = require('gulp');
 require('./client/gulpfile.js');
 
 gulp.task('build', gulp.series('client-build'));
+
+var istanbul = require('gulp-istanbul');
+var mocha = require('gulp-mocha');
+function test() {
+  return gulp.src('test/**/*.js')
+    .pipe(mocha({reporter: 'progress'}));
+};
+
+gulp.task('test', test);
+
 gulp.task('default', gulp.series('build'));
