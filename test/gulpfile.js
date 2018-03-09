c366008fead37a416c62f26987a9894dd24d4941
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2018-03-08 22:53:39 -0500

    Integrated istanbul into gulp.

--- test/gulpfile.js
+++ test/gulpfile.js
@@ -1,11 +1,19 @@
 var gulp = require('gulp');
 var istanbul = require('gulp-istanbul');
 var mocha = require('gulp-mocha');
+var path = require('path');
 
-function testServer() {
-  return gulp.src('server/**/*.js', {cwd: __dirname})
-    .pipe(mocha({reporter: 'progress'}));
+function serverPreTest() {
+  return gulp.src(['../server/**/*.js'], {cwd: __dirname})
+    .pipe(istanbul({includeUntested: true}))
+    .pipe(istanbul.hookRequire());
 };
 
-gulp.task('server-test', testServer);
+function serverTest() {
+  return gulp.src('./server/**/*.js', {cwd: __dirname})
+    .pipe(mocha({reporter: 'progress'}))
+    .pipe(istanbul.writeReports({dir: path.join(__dirname, 'coverage')}));
+};
+
+gulp.task('server-test', gulp.series(serverPreTest, serverTest));
 gulp.task('test', gulp.series('server-test'));
